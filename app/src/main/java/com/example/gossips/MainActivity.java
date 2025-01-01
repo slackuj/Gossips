74465f26d189da439aed57ba9fbeb74c082a0ef9
Umesh Joshi <slackuj@gmail.com>
2025-01-01 18:38:28 +0545

    Updates

--- app/src/main/java/com/example/gossips/MainActivity.java
+++ app/src/main/java/com/example/gossips/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
